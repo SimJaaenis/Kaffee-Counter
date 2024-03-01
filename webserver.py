fc15ef9a9d1cd52a81d362fdc53383da157092cc
Philipp <30900810+DerReparator@users.noreply.github.com>
2024-03-01 22:23:35 +0100

    basic Captive Portal working on iPhone

--- webserver.py
+++ webserver.py
@@ -1,6 +1,6 @@
 '''the Webserver that actually serves the Kaffee information.'''
 
-from phew import server
+from phew import logging, server
 from phew.template import render_template
 from phew.server import redirect
 
@@ -9,17 +9,52 @@ import time
 
 DOMAIN: str = "kaffee-counter.local"
 
-html: str
-with open('resources/index.html', "r", encoding="utf-8") as f:
-    html = f.read()
+@server.route("/", methods=['GET'])
+def index(request):
+    """ Render the Index page"""
+    if request.method == 'GET':
+        logging.debug("Get request")
+        return render_template("resources/index.html")
 
-if len(html) < 100:
-    raise FileNotFoundError("HTML could not be found.")
+# microsoft windows redirects
+@server.route("/ncsi.txt", methods=["GET"])
+def hotspot_win_1(request):
+    print(request)
+    print("ncsi.txt")
+    return "", 200
+
+
+@server.route("/connecttest.txt", methods=["GET"])
+def hotspot_win_2(request):
+    print(request)
+    print("connecttest.txt")
+    return "", 200
+
+
+@server.route("/redirect", methods=["GET"])
+def hotspot_win_3(request):
+    print(request)
+    print("****************ms redir*********************")
+    return redirect(f"http://{DOMAIN}/", 302)
+
+# android redirects
+@server.route("/generate_204", methods=["GET"])
+def hotspot_android(request):
+    print(request)
+    print("******generate_204********")
+    return redirect(f"http://{DOMAIN}/", 302)
+
+# apple redir
+@server.route("/hotspot-detect.html", methods=["GET"])
+def hotspot_apple(request):
+    """ Redirect to the Index Page """
+    print(request)
+    return render_template("index.html")
 
 @server.catchall()
 def catchall(request):
     '''The catchall implementations'''
-    return "Der Kaffee ist ausgelaufen. Nichts gefunden!", 404
+    return redirect(f"http://{DOMAIN}/")
 
 def init_webserver():
     '''Initialisiere den Webserver.'''
